619da0d34075bdf6c8dfb664b117563d106114e8
Peter Drake <drake@lclark.edu>
2017-01-10 13:56:19 -0800

    Now handles words with quotes.

--- cap.py
+++ cap.py
@@ -8,5 +8,7 @@ Created on Tue Jan 10 12:03:28 2017
 
 # Using an example from Lubanovic, Introducing Python, p. 313
 def just_do_it(text):
-    from string import capwords
-    return capwords(text)
+    import re
+    segments = re.split(r'([^a-zA-z\']+)', text)  
+    return ''.join(a.capitalize() + b.capitalize()
+                    for a, b in zip(segments[::2], segments[1::2] + ['']))

8c1417fcd6ce7d701ebeaaa949dcfa5bee8e1d7d
Peter Drake <drake@lclark.edu>
2017-01-10 12:06:26 -0800

    Created cap.py. Working through an example from Lubanovic, Introducing Python, p. 315.

---
+++ cap.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 12:03:28 2017
+
+@author: drake
+"""
+
+# Using an example from Lubanovic, Introducing Python, p. 313
+def just_do_it(text):
+    return text.capitalize()

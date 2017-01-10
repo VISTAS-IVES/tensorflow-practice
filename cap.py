bfe1adf9c3f81801dea59e2dad6d8e7153172fb7
Peter Drake <drake@lclark.edu>
2017-01-10 12:06:47 -0800

    Fixed comment typo.

--- cap.py
+++ cap.py
@@ -6,6 +6,6 @@ Created on Tue Jan 10 12:03:28 2017
 @author: drake
 """
 
-# Using an example from Lubanovic, Introducing Python, p. 313
+# Using an example from Lubanovic, Introducing Python, p. 315
 def just_do_it(text):
     return text.capitalize()
